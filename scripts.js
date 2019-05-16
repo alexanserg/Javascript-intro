a2bc2b3fb27573488e5dddec976574768c38738c
Alexander Sergejev and Ian Alexander <ian@>
2019-05-15 19:57:31 -0700

    it will create html with javascrpit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// alert("This will affect the disposition of your soul!");
+
+var name = prompt("What is your name?");
+
+var food = prompt("what is your favorite food?")
+
+var age = prompt("what is your age?")
+
+var saySomething = function(whatNotToSay) {
+  alert(whatNotToSay)
+};
+
+saySomething("My name is " + name + ", I am " +  age + " years old, and my favorite food is " + food + ".");
