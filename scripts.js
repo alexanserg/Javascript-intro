865fa8acf41fc9fc48e2322c857e851128b9e89e
Alexander Sergejev and Ian Alexander <ian@>
2019-05-15 20:57:09 -0700

    make a calculator

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// alert("This will affect the disposition of your soul!");
+alert("This will affect the disposition of your soul!");
 
 var name = prompt("What is your name?");
 
@@ -11,3 +11,31 @@ var saySomething = function(whatNotToSay) {
 };
 
 saySomething("My name is " + name + ", I am " +  age + " years old, and my favorite food is " + food + ".");
+
+
+var add = function(first, second) {
+  return first + second;
+};
+
+var subtract= function(first, second) {
+  return first - second;
+};
+
+var divide = function(first, second) {
+  return first / second;
+};
+
+var multiply = function(first, second) {
+  return first * second;
+};
+
+var first = parseInt(prompt("pick a number"));
+
+var second = parseInt(prompt("pick another number"));
+
+
+
+alert(add(first, second));
+alert(subtract(first, second));
+alert(divide(first, second));
+alert(multiply(first, second));
